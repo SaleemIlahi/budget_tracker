6c0bd6741ed8eecdcf6dba16663b5dff5f4bb099
Mohamed Saleem <saleemilahi21@gmail.com>
2025-10-28 20:36:41 +0530

    feat(client): setup main App structure and global styles

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import S from "./App.module.scss";
+import Page from "./Page";
+
+function App() {
+  return (
+    <div className={S.App}>
+      <Page />
+    </div>
+  );
+}
+
+export default App;
