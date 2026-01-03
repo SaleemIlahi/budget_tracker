d3583eaa3802b93724eecf030130b9dca803a186
Mohamed Saleem <saleemilahi21@gmail.com>
2026-01-03 14:54:09 +0530

    feat: wrap app with AuthProvider and DataProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import { DataProvider } from "./context/DataContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
