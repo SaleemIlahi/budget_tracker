730c63c736edff9770cf020b3a1d6cd7af8df90d
Mohamed Saleem <saleemilahi21@gmail.com>
2026-01-03 14:53:01 +0530

    feat: add ProtectedRoutes for route protection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,52 @@
+import { useEffect } from "react";
 import S from "./App.module.scss";
 import Page from "./Page";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { useApi } from "./hook/useApi";
+import { useAuth } from "./context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import Loading from "./components/Loading";
 
 function App() {
+  interface Data {
+    name: string;
+    email: string;
+  }
+  interface Response {
+    status: number;
+    data: Data;
+    message: string;
+  }
+  const navigate = useNavigate();
+  const { dispatch, loading } = useAuth();
+  const { request } = useApi();
+  useEffect(() => {
+    const checkUser = async () => {
+      const res = await request<Response>("GET", "refresh");
+      if (res.status === 200) {
+        dispatch({ type: "LOGIN", payload: res.data });
+        navigate("/dashboard");
+      } else {
+        dispatch({ type: "LOADING_OFF" });
+      }
+      console.log(res);
+    };
+    checkUser();
+  }, []);
+
   return (
     <div className={S.App}>
-      <Page />
+      {loading && <Loading />}
+      <Routes>
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<Register />} />
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Page />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
