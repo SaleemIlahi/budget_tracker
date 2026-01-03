8dc13977b2bdefb788fd36a037b2ae6752e1d4f3
Mohamed Saleem <saleemilahi21@gmail.com>
2026-01-03 14:39:36 +0530

    refactor: convert props type to generic and add fallback styles

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,36 +1,42 @@
-import React from "react";
-import "../style/table.module.scss";
+import S from "../style/table.module.scss";
 
-interface Body {
-  [key: string]: string | number;
-}
-
-interface Tableprops {
+interface Tableprops<T extends object> {
   head: { id: string; name: string }[];
-  body: Body[];
+  body: T[];
 }
 
-const Table: React.FC<Tableprops> = (props) => {
+const Table = <T extends object>(props: Tableprops<T>) => {
   const { head, body } = props;
   return (
-    <table>
-      <thead>
-        <tr>
-          {head.map((o) => (
-            <th key={o.id}>{o.name}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {body.map((o, i) => (
-          <tr key={"__row__" + i + o.length}>
-            {Object.values(o).map((c) => (
-              <td key={c}>{c}</td>
+    <>
+      <table>
+        <thead>
+          <tr>
+            {head.map((o) => (
+              <th key={o.id}>{o.name}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {body.map((o, i) => (
+            <tr key={"__row__" + i}>
+              {Object.values(o).map((c) => (
+                <td key={c}>{c}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {body.length === 0 && (
+        <div className={S.fallback}>
+          <img
+            src="https://res.cloudinary.com/do63p55lo/image/upload/v1762679316/budget_tracker/no-data_c2wtuz.svg"
+            alt="no-data"
+          />
+          <h3>No Data Found</h3>
+        </div>
+      )}
+    </>
   );
 };
 
